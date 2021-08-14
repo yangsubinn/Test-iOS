1679be8169125d28f5a991670c435ca0ac3e5b24
yangsubinn <cindy010670@gmail.com>
2021-08-15 00:13:50 +0900

    Haptic Vibration

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpFirstButton(_ sender: UIButton) {
+        /// 약하고 짧은 진동
+        AudioServicesPlaySystemSound(1519)
+    }
+    
+    
+    @IBAction func touchUpSecondButton(_ sender: UIButton) {
+        /// 덜 약하고 짧은 진동
+        AudioServicesPlaySystemSound(1520)
+    }
+    
+    @IBAction func touchUpThirdButton(_ sender: UIButton) {
+        /// nope 두 번 진동
+        AudioServicesPlaySystemSound(1521)
+    }
+    
+    @IBAction func touchUpFourthButton(_ sender: UIButton) {
+        /// 쎄고 긴 진동
+        AudioServicesPlaySystemSound(4095)
+    }
 }
 
