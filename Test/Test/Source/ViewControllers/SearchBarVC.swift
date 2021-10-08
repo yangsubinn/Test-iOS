86b1296535e22ad328734c4435ea6d53f5bc5791
yangsubinn <cindy010670@gmail.com>
2021-10-09 04:30:37 +0900

    SearchBarVC 추가

---
+++ Test/Test/Source/ViewControllers/SearchBarVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchBarVC.swift
+//  Test
+//
+//  Created by 양수빈 on 2021/10/09.
+//
+
+import UIKit
+
+class SearchBarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
