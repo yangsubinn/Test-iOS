8646213c3fb04c43344a9f8b917173a5a753eb78
yangsubinn <cindy010670@gmail.com>
2021-11-18 18:50:48 +0900

    [Feat] CollectionView header 구현

--- Test/Test/Source/ViewControllers/Header/Cell/HeaderReusableView.swift
+++ Test/Test/Source/ViewControllers/Header/Cell/HeaderReusableView.swift
@@ -8,5 +8,33 @@
 import UIKit
 
 class HeaderReusableView: UICollectionReusableView {
+    
+    static let identifier = "HeaderReusableView"
+    
+    let titleLabel = UILabel()
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
+        configUI()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configUI() {
+        self.backgroundColor = .purple
+        titleLabel.text = "HeaderReusableView"
+        titleLabel.textColor = .white
+    }
+    
+    func setupLayout() {
+        addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
 }
--- Test/Test/Source/ViewControllers/Header/HeaderVC.swift
+++ Test/Test/Source/ViewControllers/Header/HeaderVC.swift
@@ -7,23 +7,95 @@
 
 import UIKit
 
-class HeaderVC: UIViewController {
+import Then
+import SnapKit
 
+class HeaderVC: UIViewController {
+    
+    // MARK: - Properties
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+    
+    var titles = ["제목1", "제목2", "제목3", "제목4", "제목5", "제목6", "제목이 길면", "바나나", "바나나는 맛있어"]
+    
+    let collectionViewFlowLayout = UICollectionViewFlowLayout()
+
+    // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupLayout()
+        setupDelegate()
+        registerNib()
+    }
+
+    // MARK: - Custom Method
+    func setupDelegate() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func registerNib() {
+        collectionView.register(HeaderReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "HeaderReusableView")
+        collectionView.register(HeaderCVC.self, forCellWithReuseIdentifier: "HeaderCVC")
     }
-    */
+    
+    func setupLayout() {
+        view.addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func configUI() {
+        collectionViewFlowLayout.scrollDirection = .horizontal
+    }
+}
 
+// MARK: - UICollectionViewDelegate
+extension HeaderVC: UICollectionViewDelegate {
+    
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension HeaderVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 100)
+    }
+    
+    /// 세로 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension HeaderVC: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HeaderCVC", for: indexPath) as? HeaderCVC else {return UICollectionViewCell()}
+        
+        cell.titleLabel.text = titles[indexPath.item]
+                
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        /// header
+        guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "HeaderReusableView", for: indexPath) as? HeaderReusableView else { return UICollectionReusableView() }
+
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 200)
+    }
 }
