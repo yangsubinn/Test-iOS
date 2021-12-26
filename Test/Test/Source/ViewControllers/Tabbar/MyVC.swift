0a56b9d411ab0bc45829eb212c0acebddd8cbda2
yangsubinn <cindy010670@gmail.com>
2021-12-26 15:53:58 +0900

    [Add] Tabbar programmatically

---
+++ Test/Test/Source/ViewControllers/Tabbar/MyVC.swift
@@ -0,0 +1,14 @@
+//
+//  MyVC.swift
+//  Test
+//
+//  Created by 양수빈 on 2021/12/26.
+//
+
+import UIKit
+
+class MyVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Test/Test/Source/ViewControllers/Tabbar/OtherVC.swift
@@ -0,0 +1,14 @@
+//
+//  OtherVC.swift
+//  Test
+//
+//  Created by 양수빈 on 2021/12/26.
+//
+
+import UIKit
+
+class OtherVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
