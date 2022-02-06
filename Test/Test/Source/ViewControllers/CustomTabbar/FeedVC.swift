de08c8bf721f0b324ce41c8fe09cc4d1dd82e813
yangsubinn <cindy010670@gmail.com>
2022-02-06 17:12:09 +0900

    [Feat] CustomTabbar

---
+++ Test/Test/Source/ViewControllers/CustomTabbar/FeedVC.swift
@@ -0,0 +1,29 @@
+//
+//  FeedVC.swift
+//  Test
+//
+//  Created by 양수빈 on 2022/01/31.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
