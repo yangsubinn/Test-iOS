b122a199b9b7a6cfe2d3a5b1779051ee1e93c049
yangsubinn <cindy010670@gmail.com>
2022-02-07 00:23:34 +0900

    [Add] FloatingButton 프로젝트 세팅

---
+++ FloatingButton/FloatingButton/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FloatingButton
+//
+//  Created by 양수빈 on 2022/02/07.
+//
+
+import UIKit
+
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
