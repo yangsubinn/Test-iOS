49edaf4e32346435f5d519c380eec3b05d1eb8ef
yangsubinn <cindy010670@gmail.com>
2022-02-07 23:08:34 +0900

    [Chore] 수정한 floatingButton 라이브러리 적용

--- FloatingButton/FloatingButton/ViewController.swift
+++ FloatingButton/FloatingButton/ViewController.swift
@@ -8,14 +8,42 @@
 import UIKit
 
 import SnapKit
+import JJFloatingActionButton
 
 class ViewController: UIViewController {
+    
+    let actionButton = JJFloatingActionButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setLayout()
+        setButtonUI()
     }
-
-
+    
+    func setLayout() {
+        view.addSubview(actionButton)
+        
+        actionButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(30)
+            make.bottom.equalToSuperview().inset(30)
+        }
+    }
+    
+    func setButtonUI() {
+        actionButton.buttonImage = UIImage(systemName: "moon")
+        actionButton.buttonColor = .purple
+        actionButton.buttonImageColor = .white
+        actionButton.buttonImageSize = CGSize(width: 24, height: 24)
+        actionButton.itemSizeRatio = CGFloat(1)
+        
+        actionButton.addItem(title: "첫번째", image: UIImage(systemName: "house")) { _ in
+            print("첫번째 눌림")
+        }
+        
+        actionButton.addItem(title: "두번째", image: UIImage(systemName: "house.fill")) { _ in
+            print("두번째 눌림")
+        }
+    }
 }
 
