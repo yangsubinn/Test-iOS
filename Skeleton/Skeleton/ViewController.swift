aa487b47e04da3174de5199e3028011785876553
Subin Yang <cindy010670@gmail.com>
2022-03-12 22:48:40 +0900

    [Add] Skeleton 프로젝트 세팅

---
+++ Skeleton/Skeleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skeleton
+//
+//  Created by 양수빈 on 2022/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
