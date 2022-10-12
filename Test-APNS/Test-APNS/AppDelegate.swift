e5febcd189d5dcf0aebf67b2ab1ad93a97c6fc49
Subin Yang <cindy010670@gmail.com>
2022-10-12 17:21:58 +0900

    [Feat] 로컬 푸시알림

---
+++ Test-APNS/Test-APNS/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  Test-APNS
+//
+//  Created by 양수빈 on 2022/10/12.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().delegate = self // 푸시알림 권한 설정
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // 앱이 실행 중(foreground)에 푸시을 받는 경우 처리하는 메소드
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
+}
+
---
+++ Test-APNS/Test-APNS/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Test-APNS
+//
+//  Created by 양수빈 on 2022/10/12.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.requestNotificationAuthorization()
+    }
+    
+    /// 푸시알림 권한 허용 요청
+    func requestNotificationAuthorization() {
+        let authOptions: UNAuthorizationOptions = [.alert, .sound, .badge]
+        
+        notificationCenter.requestAuthorization(options: authOptions) { success, error in
+            if let error = error {
+                print(error)
+            }
+        }
+    }
+
+    /// 자체 푸시 알림
+    @IBAction func pushButtonTapped(_ sender: Any) {
+        print("pushButtonTapped")
+        notificationCenter.removeAllPendingNotificationRequests()
+        
+        let push = UNMutableNotificationContent()
+        push.title = "title임"
+        push.subtitle = "subtitle임"
+        push.body = "bodybody"
+//        push.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: push, trigger: trigger)
+        notificationCenter.add(request, withCompletionHandler: nil)
+    }
+    
+}
+
