87beaf29d0e6c4f2dc9ee0bc6f4a0719219b26ce
Subin Yang <cindy010670@gmail.com>
2022-10-12 15:13:11 +0900

    [Feat] SFSafariViewController

--- WebView/WebView/ViewController.swift
+++ WebView/WebView/ViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import SnapKit
+import SafariServices
 
 class ViewController: UIViewController {
     
     private let stackView = UIStackView()
     private let webViewButton = UIButton()
     private let safariButton = UIButton()
+    private let safariVCButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +30,9 @@ class ViewController: UIViewController {
         
         safariButton.setTitle("SafariApp", for: .normal)
         safariButton.setTitleColor(.blue, for: .normal)
+        
+        safariVCButton.setTitle("SafariViewController", for: .normal)
+        safariVCButton.setTitleColor(.blue, for: .normal)
     }
     
     private func setStackView() {
@@ -44,11 +49,13 @@ class ViewController: UIViewController {
         
         stackView.addArrangedSubview(webViewButton)
         stackView.addArrangedSubview(safariButton)
+        stackView.addArrangedSubview(safariVCButton)
     }
 
     private func setAddTarget() {
         webViewButton.addTarget(self, action: #selector(presentWKWebVC), for: .touchUpInside)
         safariButton.addTarget(self, action: #selector(moveToSafari), for: .touchUpInside)
+        safariVCButton.addTarget(self, action: #selector(openSafariVC), for: .touchUpInside)
     }
     
     @objc
@@ -62,6 +69,13 @@ class ViewController: UIViewController {
     private func moveToSafari() {
         let url = URL(string: "https://nosy-repair-8a6.notion.site/Web-views-b0673c13e9564ecfbe880481ab74454b")
         UIApplication.shared.open(url!)
+    }
+    
+    @objc
+    private func openSafariVC() {
+        let url = URL(string: "https://nosy-repair-8a6.notion.site/Web-views-b0673c13e9564ecfbe880481ab74454b")
+        let safariVC = SFSafariViewController(url: url!)
+        present(safariVC, animated: true)
     }
 }
 
