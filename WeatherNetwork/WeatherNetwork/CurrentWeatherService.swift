565db712f659c4432749a1a5f8bfd4eb7ffe4657
Subin Yang <cindy010670@gmail.com>
2022-11-10 02:51:32 +0900

    [Add] WeatherNetwork - URLSession

---
+++ WeatherNetwork/WeatherNetwork/CurrentWeatherService.swift
@@ -0,0 +1,115 @@
+//
+//  CurrentWeatherService.swift
+//  WeatherNetwork
+//
+//  Created by 양수빈 on 2022/11/10.
+//
+
+import Foundation
+
+
+struct CurrentWeatherService {
+    static let shared = CurrentWeatherService()
+    
+    // 1. create url
+    let urlString = "https://api.openweathermap.org/data/2.5/weather"
+    let appid = "118e38c88f78ef1eb986616b22f12fec"
+    let lat = "37.5666791"
+    let lon = "126.9782914"
+    let units = "metric"
+    
+    func fetchCurrentWeatherData(completion: @escaping (Result<Any, Error>) -> ()) {
+        guard var urlComponents = URLComponents(string: urlString) else { return }
+        
+        let query: [String: String] = [
+            "appid": appid,
+            "lat": lat,
+            "lon": lon,
+            "units": units
+        ]
+        
+        let queryItemArray = query.map {
+            URLQueryItem(name: $0.key, value: $0.value)
+        }
+        urlComponents.queryItems = queryItemArray
+        
+        guard let requestURL = urlComponents.url else { return }
+        
+        let session = URLSession(configuration: .default)
+        let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            if let safeData = data {
+                do {
+                    let decodedData = try JSONDecoder().decode(CurrentWeatherEntity.self, from: safeData)
+                    completion(.success(decodedData))
+                } catch let DecodingError.dataCorrupted(context) {
+                    print(context)
+                } catch let DecodingError.keyNotFound(key, context) {
+                    print("Key '\(key)' not found:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } catch let DecodingError.valueNotFound(value, context) {
+                    print("Value '\(value)' not found:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } catch let DecodingError.typeMismatch(type, context)  {
+                    print("Type '\(type)' mismatch:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } catch {
+                    print("error: ", error)
+                }
+                
+            }
+        }
+        dataTask.resume()
+        
+//        if let url = URL(string: urlString) {
+//            // 2. create urlsession
+//            // URLSession 유형: default(기본), ephemeral(쿠키나 캐시 X), background(백그라운드에서 다운로드/업로드)
+//            let session = URLSession(configuration: .default)
+//
+//            var requestURL = URLRequest(url: url)
+//            // header
+//            requestURL.setValue(appid, forHTTPHeaderField: "appid")
+//            requestURL.setValue(lat, forHTTPHeaderField: "lat")
+//            requestURL.setValue(lon, forHTTPHeaderField: "lon")
+//            requestURL.setValue(units, forHTTPHeaderField: "units")
+//
+//            // 3. give urlsession task
+//            // SessionTask 작업 유형: dataTask, uploadTask, downloadTask
+//            let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
+//                if error != nil {
+//                    print(error!)
+//                    return
+//                }
+//
+//                if let safeData = data {
+//                    do {
+//                        print("safeData", safeData)
+//                        let decodedData = try JSONDecoder().decode(CurrentWeatherEntity.self, from: safeData)
+//                        print("decodedData", decodedData)
+//                        completion(.success(decodedData))
+//                    } catch let DecodingError.dataCorrupted(context) {
+//                        print(context)
+//                    } catch let DecodingError.keyNotFound(key, context) {
+//                        print("Key '\(key)' not found:", context.debugDescription)
+//                        print("codingPath:", context.codingPath)
+//                    } catch let DecodingError.valueNotFound(value, context) {
+//                        print("Value '\(value)' not found:", context.debugDescription)
+//                        print("codingPath:", context.codingPath)
+//                    } catch let DecodingError.typeMismatch(type, context)  {
+//                        print("Type '\(type)' mismatch:", context.debugDescription)
+//                        print("codingPath:", context.codingPath)
+//                    } catch {
+//                        print("error: ", error)
+//                    }
+//
+//                }
+//            }
+//            // 4. start task
+//            dataTask.resume()
+//        }
+    }
+}
---
+++ WeatherNetwork/WeatherNetwork/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  WeatherNetwork
+//
+//  Created by 양수빈 on 2022/11/08.
+//
+
+import UIKit
+
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    private let currentTempLabel = UILabel()
+    private let minTempLabel = UILabel()
+    private let maxTempLabel = UILabel()
+    private var currentWeatherData: CurrentWeatherEntity!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setLayout()
+        getCurrentWeather()
+    }
+
+    private func setUI() {
+        currentTempLabel.font = .boldSystemFont(ofSize: 24)
+    }
+
+    private func setLayout() {
+        [currentTempLabel, minTempLabel, maxTempLabel].forEach { view.addSubview($0) }
+        
+        currentTempLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.centerY.equalTo(view.safeAreaLayoutGuide).offset(-40)
+        }
+        
+        minTempLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide).offset(-60)
+            make.top.equalTo(currentTempLabel.snp.bottom).offset(40)
+        }
+        
+        maxTempLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide).offset(60)
+            make.centerY.equalTo(minTempLabel)
+        }
+    }
+    
+    private func setData(current: Double, max: Double, min: Double) {
+        currentTempLabel.text = "\(String(current))°C"
+        maxTempLabel.text = "\(String(max))°C"
+        minTempLabel.text = "\(String(min))°C"
+    }
+}
+
+// MARK: - Network
+
+extension ViewController {
+    func getCurrentWeather() {
+        CurrentWeatherService.shared.fetchCurrentWeatherData { response in
+            switch response {
+            case .success(let data):
+                print("✅ getCurrentWeather - ",data)
+                self.currentWeatherData = data as? CurrentWeatherEntity
+                if let data = data as? CurrentWeatherEntity {
+                    // ui 변경은 main queue에서 (서버 통신이 global queue로 넘어가서 진행됨)
+                    DispatchQueue.main.async {
+                        let tempData = data.main
+                        self.setData(current: tempData.temp, max: tempData.tempMax, min: tempData.tempMin)
+                    }
+                }
+            case .failure(let err):
+                print("❌ getCurrentWeather - ",err)
+            }
+        }
+    }
+}
