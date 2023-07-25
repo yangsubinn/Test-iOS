a2c50043a31edf538d432364786a0d06fefdfed1
Subin Yang <cindy010670@gmail.com>
2023-07-25 14:23:27 +0900

    [Add] 애플 로그인

--- SocialLogin/SocialLogin/ViewController.swift
+++ SocialLogin/SocialLogin/ViewController.swift
@@ -6,17 +6,19 @@
 //
 
 import UIKit
+import AuthenticationServices
 import KakaoSDKUser
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var kakaoLoginButton: UIButton!
+    @IBOutlet weak var appleLoginButton: ASAuthorizationAppleIDButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        appleLoginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
     }
-
     
     @IBAction func kakaoLoginButtonTapped(_ sender: Any) {
         if UserApi.isKakaoTalkLoginAvailable() {
@@ -31,5 +33,57 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    @objc
+    func loginButtonTapped() {
+        print("loginButtonTapped")
+        let appleIDProvide = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvide.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
 }
 
+extension ViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        print("loginWithApple success 🍎")
+        
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            if let authorizationCode = appleIDCredential.authorizationCode,
+               let identityToken = appleIDCredential.identityToken,
+               let authCodeString = String(data: authorizationCode, encoding: .utf8),
+               let identifyTokenString = String(data: identityToken, encoding: .utf8) {
+                print("🧃authorizationCode: \(authorizationCode)")
+                print("identityToken: \(identityToken)")
+                print("authCodeString: \(authCodeString)")
+                print("identifyTokenString: \(identifyTokenString)")
+            }
+            print("useridentifier: \(userIdentifier), fullName: \(fullName), email: \(email)")
+        case let passwordCredential as ASPasswordCredential:
+            let username = passwordCredential.user
+            let password = passwordCredential.password
+            
+            print("🧃username: \(username)")
+            print("password: \(password)")
+        default:
+            break
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("apple login failed \(error.localizedDescription)")
+    }
+}
